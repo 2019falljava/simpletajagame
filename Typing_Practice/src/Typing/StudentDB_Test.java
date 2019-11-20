c8aec2d7d636d5758770c80febb90f82a7ad63bd
moonk1212 <zm8208@naver.com>
2019-11-21 00:30:22 +0900

    db부분 commit 로컬 아이디랑 비번 스키마는 자신의 디비에 맞춰서해야함!

---
+++ Typing_Practice/src/Typing/StudentDB_Test.java
@@ -0,0 +1,37 @@
+package Typing;
+
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDB_Test {
+ 
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        StudentDao sDao = new StudentDao();
+        Student student = new Student();
+      /*//insert DB 테스트
+        student.setId(1);
+        student.setGrade(123);
+        student.setName("sds");
+        sDao.insertStudent(student);
+        
+        
+  */
+        
+
+        //selectAll DB 테스트
+        List<Student> list = sDao.selectAll();
+        
+      //결과물 출력
+        for(int i=0; i<list.size(); i++) {
+        	System.out.println("회원아이디:"+list.get(i).getId()); 
+        	System.out.println("회원이름:"+list.get(i).getName()); 
+        	System.out.println("회원점수:"+list.get(i).getGrade());
+        	System.out.println("----------------------------------");
+        }
+
+          
+    }
+}
+
---
+++ Typing_Practice/src/Typing/StudentDao.java
@@ -0,0 +1,97 @@
+package Typing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+ 
+
+public class StudentDao {
+	 
+private Connection conn;
+private static final String USERNAME = "아이디";
+private static final String PASSWORD = "비번";
+private static final String URL = "jdbc:mysql://localhost:3306/스키마이름?useSSL=false";
+
+public StudentDao() {
+    // connection객체를 생성해서 디비에 연결해줌..
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+    } catch (ClassNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        System.out.println("클래스 적재 실패!!");
+    } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        System.out.println("연결 실패!!");
+    }
+}
+
+public void insertStudent(Student student) {
+    String sql = "insert into student(name,grade) values(?,?);";
+    PreparedStatement pstmt = null;
+    String name;
+    int grade;
+    name=student.getName();
+    grade=student.getGrade();
+    try {
+        pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1,name);
+        pstmt.setInt(2, grade);
+        pstmt.executeUpdate();
+    } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } finally {
+        try {
+            if (pstmt != null && !pstmt.isClosed())
+                pstmt.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
+
+public List<Student> selectAll() {
+    String sql = "select * from student order by grade;";//DB 상에서 grade로 정렬하기
+    PreparedStatement pstmt = null;
+
+    List<Student> list = new ArrayList<Student>();
+
+    try {
+        pstmt = conn.prepareStatement(sql);
+        ResultSet re = pstmt.executeQuery();
+
+        while (re.next()) {
+            Student s = new Student();
+            s.setId(re.getInt("id"));
+            s.setName(re.getString("name"));
+            s.setGrade(re.getInt("grade"));
+            list.add(s);
+        }
+
+    } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } finally {
+        try {
+            if (pstmt != null && !pstmt.isClosed())
+                pstmt.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    return list;
+}
+}
+
+
